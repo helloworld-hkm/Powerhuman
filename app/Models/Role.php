4eb7c69d1286dbb38d41fe171edf09f858902329
helloworld-hkm <81341249+helloworld-hkm@users.noreply.github.com>
2022-12-16 00:50:40 +0700

    session 7
    seeder

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -25,7 +25,7 @@ class Role extends Model
     }
     public function employees()
     {
-        // return $this->hasMany(Employee::class);
+        return $this->hasMany(Employee::class);
     }
     public function responsibilities()
     {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
+use App\Models\Role;
+use App\Models\Team;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +21,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
+        // Company::factory(10)->create();
+        // Team::factory(30)->create();
+        // Role::factory(10)->create();
+        // Responsibility::factory(200)->create();
+        // Employee::factory(1000)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
